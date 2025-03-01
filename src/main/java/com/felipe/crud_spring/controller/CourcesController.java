2457b72f272f4b3239548263d974d3abf335f24e
felipeggs <felipesidy9@gmail.com>
2025-03-01 15:59:55 -0300

    Alteração 01-03

--- src/main/java/com/felipe/crud_spring/controller/CourcesController.java
+++ src/main/java/com/felipe/crud_spring/controller/CourcesController.java
@@ -6,13 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.felipe.crud_spring.model.Course;
+import com.felipe.crud_spring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CourcesController {
+    
+    private final CourseRepository courseRepository;
 
     //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public List<Object> list(){
-        return null;
+    public List<Course> list(){
+        return courseRepository.findAll();
     }
+
+
 }
---
+++ src/main/java/com/felipe/crud_spring/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.felipe.crud_spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipe.crud_spring.model.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository< Course, Long>{//Declarar entidade e seu tipo
+
+
+}
